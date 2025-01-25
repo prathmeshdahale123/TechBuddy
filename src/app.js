bc348b22278d1ff49f9b057aeac4f87798799a99
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-01-25 19:36:19 +0530

    installed mongoose

--- src/app.js
+++ src/app.js
@@ -1,31 +1,19 @@
 const express = require('express');
-
+const { connectDB } = require("./config/database")
 const app = express();
-const { isAdminAuthorised, isUserAuthorized } = require("./middleware/auth")
 
-app.use("/admin", isAdminAuthorised)
-
-app.get("/user/data", isUserAuthorized, (req,res) => {
-    res.send("user data send")
-})
-app.get("/user/login", (req,res) => {
-    res.send("User logged in succesfully.")
-})
-
-app.get("/admin/getdata", (req,res) => {
-    res.send("all data fetched");
-})
-app.get("/admin/delete", (req,res) => {
-    res.send("user deleted succesfully");
-})
-
-// Error handling middleware - used to handle all the errors
-app.use("/", (err,req,res,next) => {
-    res.status(500).send("Something went wrong");
-})
+connectDB()
+.then(() => {
+    console.log("database connected succesfully...");
+    app.listen(7777, () => {
+        console.log("Server is listening on port 7777");
+    })
+    
+}).catch(() => {
+    console.log("database connection failed!!!");
+    
+} )
 
 
 
-app.listen(7777, () => {
-    console.log("Server is listening on port 7777");
-})
+
