e1ed830efcf824ec7f2e5bf0d3db6370c45180ab
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-01-25 13:50:05 +0530

    playing with various middlewares

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 
 const app = express();
+const { isAdminAuthorised, isUserAuthorized } = require("./middleware/auth")
 
-app.use("/user", (req, res, next) => {
-    next();
-    
-}, (req, res, next) => {
-    next();
-    
-}, (req, res, next) => {
-    res.send("response 3");
-    next();
-}
-) 
+app.use("/admin", isAdminAuthorised)
+
+app.get("/user/data", isUserAuthorized, (req,res) => {
+    res.send("user data send")
+})
+app.get("/user/login", (req,res) => {
+    res.send("User logged in succesfully.")
+})
+
+app.get("/admin/getdata", (req,res) => {
+    res.send("all data fetched");
+})
+app.get("/admin/delete", (req,res) => {
+    res.send("user deleted succesfully");
+})
+
+
 
 app.listen(7777, () => {
     console.log("Server is listening on port 7777");
