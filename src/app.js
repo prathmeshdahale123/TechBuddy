5c78697a084a041a6937b1687c545af376494ae7
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-01-28 13:03:09 +0530

    Login API

--- src/app.js
+++ src/app.js
@@ -2,20 +2,60 @@ const express = require('express');
 const { connectDB } = require("./config/database")
 const app = express();
 const { User } = require("./models/user")
+const { validateData } = require("./utils/validate")
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+
 
 app.use(express.json());
 
 //API route to create new user
 app.post("/signup", async (req,res) => {
-    const user = new User(req.body);
-
     try {
+    //validate data
+    validateData(req)
+
+    // encrypt the password
+    const { firstName, lastName, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+   
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword
+    });
+    
         await user.save();
     res.json("User created succesfully...");
     } catch (error) {
-        res.status(400).send("something went wrong in user creation." + error.message)
+        res.status(400).send("ERROR : " + error.message)
     }
 })
+
+//login API
+app.post("/login", async (req,res) => {
+    try {
+        const { email, password } = req.body;
+    if(!validator.isEmail(email)){
+        throw new Error("Invalide email format")
+    }
+    const user = await User.findOne({email})
+    if(!user) {
+        throw new Error("email not present")
+    } 
+   const isPassValid = await bcrypt.compare(password, user.password)
+   if(isPassValid) {
+     res.send("Login succesfull!")
+   }
+   else{
+        throw new Error("Invalid password")
+   }
+    } catch (error) {
+        res.status(400).send("ERROR : " + error.message)
+    }
+})
+
 //route to get user by email
 app.get("/user", async (req,res) => {
     const userEmail = req.body.email;
