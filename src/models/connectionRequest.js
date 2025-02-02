9b062247b02eddc7a940de16f14360687f5f4d6f
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-02-02 09:10:28 +0530

    connection request schema

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["interested", "ignored", "accepted", "rejected"],
+            message: '{VALUE} is not supported'
+        }
+    }
+})
+
+connectionRequestSchema.pre("save", function (next) {
+    connectionRequest = this;
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+        throw new Error("You can not send request to yourself!")
+    }
+    next();
+})
+
+const ConnectionRequestModel = mongoose.model("ConnectionRequestModel", connectionRequestSchema)
+
+module.exports = {
+    ConnectionRequestModel
+}
