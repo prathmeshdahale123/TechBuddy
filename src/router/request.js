65eb73a48e6ec15da8347b53119843e2a64d1a5f
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-03-07 14:43:19 +0530

    optimized review request API

--- src/router/request.js
+++ src/router/request.js
@@ -2,7 +2,8 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middleware/auth")
 const { ConnectionRequestModel } = require("../models/connectionRequest")
-const { User } = require("../models/user")
+const { User } = require("../models/user");
+const { append } = require("express/lib/response");
 
 
 requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req,res) => {
@@ -48,27 +49,29 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req,res) 
   //loggedInUser == elon
   //status == interested
   // requestId should be valid
+
   requestRouter.post("/request/review/:status/:requestId", userAuth, async (req, res) => {
     try {
     const loggedInUser = req.user;
     const { status, requestId } = req.params;
-    console.log('Received status:', status);
+
     const allowedStatus = ["accepted", "rejected"];
     if(!allowedStatus.includes(status)){
         return res.status(404).json({msg:"status not allowed"})
     }
     const connectionRequest = await ConnectionRequestModel.findOne({
         _id: requestId,
-        toUserId: loggedInUser,
+        toUserId: loggedInUser._id,
         status:"interested"
     })
-    if(!connectionRequest){
-        return res.status(404).json({msg:"connection request not found"})
+    if(!connectionRequest) {
+        return res.status(404).json({msg:"request not found"})
     }
     connectionRequest.status = status;
     const data = await connectionRequest.save();
 
-    res.json({msg:"connection request " + status, data})
+    res.json({msg:`Connection request ${status}`,data})
+
     }
     catch(err) {
         res.status(404).send("ERROR: " + err.message)
