946661a19ec7b72255af538f76a0f2ffb0679e64
Prathmesh Dahale <prathmeshdahale2023@gmail.com>
2025-03-09 09:35:30 +0530

    connections API

--- src/router/user.js
+++ src/router/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const { ConnectionRequestModel } = require("../models/connectionRequest");
 const userRouter = express.Router();
-const { User } = require("../models/user")
+const USER_SAFE_DATA = "firstName lastName age gender skills"
 
 //API to get all the pending requests of loggedInUser
 userRouter.get("/user/request/recieved",userAuth, async (req, res) => {
@@ -12,7 +12,7 @@ try {
     const connectionRequest = await ConnectionRequestModel.find({
         toUserId: loggedInUser._id,
         status:"interested"
-    }).populate("fromUserId", ["firstName","lastName"])
+    }).populate("fromUserId", USER_SAFE_DATA)
     res.json({
         msg:"Data fetched succesfully!",
         Data: connectionRequest
@@ -21,6 +21,36 @@ try {
 } catch (error) {
     res.status(400).send("ERROR : " + error.message)
 }
+})
+
+userRouter.get("/user/connections", userAuth, async(req,res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequestModel.find({
+            $or: [
+                {toUserId: loggedInUser._id, status:"accepted"},
+                {fromUserId: loggedInUser._id, status:"accepted"}
+            ]   
+        })
+        .populate("fromUserId", USER_SAFE_DATA )
+        .populate("toUserId", USER_SAFE_DATA)
+
+        const data = connectionRequest.map((row) => {
+            if (row.fromUserId._id.equals(loggedInUser._id)) {
+                return row.toUserId; // If the logged-in user is the sender, return the receiver
+            } else {
+                return row.fromUserId; // Otherwise, return the sender
+            }
+             
+        }
+           
+        )
+        res.json({ data })
+
+    } catch (error) {
+       res.status(400).send("ERROR " + error.message) 
+    }
 })
 
 module.exports = {
